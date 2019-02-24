8b9878e7805ef885d296eb18fec0264efe542dc0
shoooone <pride.a.s.w.12.01@gmail.com>
2019-02-24 20:24:52 +0900

    勝率を保存するよう修正

--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.janken
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -23,10 +24,34 @@ class ResultActivity : AppCompatActivity() {
 
         val result = myHand.judge(comHand)
         findViewById<TextView>(R.id.result_label).setText(result.text)
+        val history = saveData(result)
+        findViewById<TextView>(R.id.rate).text = "勝率：${history.winRate()}%"
+    }
+
+    fun saveData(result: Result): History {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        val gameCount = pref.getInt("GAME_COUNT", 0) + 1
+        editor.putInt("GAME_COUNT", gameCount)
+
+        var winCount = pref.getInt("WIN_COUNT", 0)
+        if (result == Result.WIN) {
+            winCount++
+            editor.putInt("WIN_COUNT", winCount)
+        }
+        editor.apply()
+        return History(gameCount, winCount)
+
     }
 
     fun onClickBackButton(view: View) {
         finish()
+    }
+}
+
+data class History(val totalGameCount: Int, val totalWinCount: Int){
+    fun winRate(): Double {
+        return totalWinCount.toDouble() / totalGameCount.toDouble() * 100
     }
 }
 
@@ -42,7 +67,7 @@ enum class Hand(
     PA(2, R.id.pa, R.drawable.pa, R.drawable.com_pa);
 
     fun judge(enemyHand: Hand): Result {
-        return when((enemyHand.index - index + 3) % 3){
+        return when ((enemyHand.index - index + 3) % 3) {
             0 -> Result.DRAW
             1 -> Result.WIN
             2 -> Result.LOSE
